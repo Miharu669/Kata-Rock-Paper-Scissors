b25f2734b2ded5ac4dd02b020f86a6ce7fac66b8
Vero Doel <veronicadoelfuentes@gmail.com>
2024-08-21 23:27:22 +0200

    now paper disproves Spock

--- src/main/java/dev/vero/models/Paper.java
+++ src/main/java/dev/vero/models/Paper.java
@@ -5,7 +5,7 @@ import dev.vero.application.Game;
 public class Paper implements Game {
     @Override
     public boolean beats(Game other) {
-        return other instanceof Rock;
+        return other instanceof Rock || other instanceof Spock;
     }
 
     @Override
