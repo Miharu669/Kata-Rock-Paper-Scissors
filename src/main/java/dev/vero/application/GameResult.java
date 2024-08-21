a6517866feda2fd7f429d1fb5052a1ba3fc87e05
Vero Doel <veronicadoelfuentes@gmail.com>
2024-08-21 23:11:31 +0200

    added class GameResult

---
+++ src/main/java/dev/vero/application/GameResult.java
@@ -0,0 +1,13 @@
+package dev.vero.application;
+
+public class GameResult {
+    public String winner(Game player, Game computer) {
+        if (player.getClass() == computer.getClass()) {
+            return "It's a tie!";
+        } else if (player.beats(computer)) {
+            return "Congrats, You won!";
+        } else {
+            return "You lost. Try again!";
+        }
+    }
+}
